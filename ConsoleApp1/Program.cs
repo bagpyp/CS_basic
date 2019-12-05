376c018daef73e1c607ad618d4503697b7a9a451
Robert Cunningham <robert@bagpyp.net>
2019-12-05 08:44:12 -0800

    update projects folder

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private const string Value1 = "Welcome to ACME Accounting Systems";
+        private const string Value2 = "Remember, we're \"Accounting\" on you!";
+
+        static void Main()
+        {
+            Console.WriteLine(Value1);
+            Console.WriteLine(Value2);
+            Console.ReadLine();
+        }
+    }
+}
