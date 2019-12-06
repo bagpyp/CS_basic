4cfa09d072f4cbcf63fcbc18c3cf53ec57eaa55d
Robert Cunningham <robert@bagpyp.net>
2019-12-06 11:07:40 -0800

    last one PLZ

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Robbie";
+            string quote = "\"Life is too short to eat the crust.\"";
+
+            string p = name + " said " + quote + "\n\t-Dumb People\\" + name + ".";
+            Console.WriteLine(p);
+            Console.ReadLine();
+
+            bool check = name.Contains("o") && name.EndsWith("e") && name.Length == 6 && name.ToUpper().Contains("O") && name.ToLower().StartsWith("r");
+
+            Console.WriteLine(check);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Robbie. ");
+            sb.Append("I'm a computer programmer. ");
+            sb.Append("You can pay me lots of money to write code. ");
+            sb.Append("Don't Panic!");
+
+            Console.WriteLine(sb);
+            Console.Read();
+            
+
+        }
+    }
+}
