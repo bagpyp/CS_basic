4df447aa4128b58688f1e7988d9ea6d13a776da9
Robert Cunningham <robert@bagpyp.net>
2019-12-11 15:41:38 -0800

    demonstrate StreamWriter and Logging

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //demonstrate text logging and streamwriter usage.
+            Console.WriteLine("Gimme a number");
+            string number = Console.ReadLine();
+            File.WriteAllText(@"C:/Users/rober/CS/basic/ConsoleApp16/log.txt", number);
+            using (StreamWriter file = new StreamWriter(@"C:/Users/rober/CS/basic/ConsoleApp16/log.txt", append: true))
+            {
+                file.WriteLine(" is the number you entered");
+            }
+            string message = File.ReadAllText(@"C:/Users/rober/CS/basic/ConsoleApp16/log.txt");
+            Console.WriteLine(message);
+            Console.Read();
+        }
+    }
+}
