0809450785cd8d2f9bce05a0d7883024bac84d9b
Robert Cunningham <robert@bagpyp.net>
2019-12-12 14:26:52 -0800

    added abstract class in Person.cs

--- ConsoleApp18/Person.cs
+++ ConsoleApp18/Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp6
 {
-    class Person
+    abstract class Person
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
